71e2ed641b44ab7827ea8f29f5ac033e4111b4ea
Erin Kosewic <2476990+penguinstampede@users.noreply.github.com>
2023-11-27 16:17:15 -0800

    updated to work with python 3.10

--- pure_pagination/paginator.py
+++ pure_pagination/paginator.py
@@ -1,5 +1,7 @@
-import collections
-
+try: # Python 3.10
+    from collections.abc import Iterable
+except ImportError:
+    from collections import Iterable
 from django.core.paginator import InvalidPage, EmptyPage, PageNotAnInteger
 from django.conf import settings
 
@@ -102,7 +104,7 @@ def add_page_querystring(func):
         if isinstance(result, int):
             querystring = self._other_page_querystring(result)
             return PageRepresentation(result, querystring)
-        elif isinstance(result, collections.Iterable):
+        elif isinstance(result, Iterable):
             new_result = []
             for number in result:
                 if isinstance(number, int):
